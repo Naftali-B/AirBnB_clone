c373a09b9bb5eac2efdb79ab241dfb9af15de03f
Naftali-B <nbudamba@gmail.com>
2023-12-09 16:27:30 +0300

    modules

--- models/amenity.py
+++ models/amenity.py
@@ -1,8 +1,8 @@
 #!usr/bin/python3
 """
-    module: Amenity
+    Amenity
 """
-from models.base_model import BaseModel
+from base_model import BaseModel
 
 
 class Amenity(BaseModel):
---
+++ models/city.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+    module: City
+"""
+from base_model import BaseModel
+
+
+class City(BaseModel):
+
+    state_id = ""
+    name = ""
